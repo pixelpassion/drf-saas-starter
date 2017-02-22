273860e6abe9afeebf926b030342df85e30ff96d
Jens Neuhaus <kontakt@jensneuhaus.de>
2017-02-22 13:40:25 +0100

    small dsn for sentry celery config

--- main/celery.py
+++ main/celery.py
@@ -41,7 +41,7 @@ class CeleryConfig(AppConfig):
             from raven.contrib.celery import register_signal as raven_register_signal
             from raven.contrib.celery import register_logger_signal as raven_register_logger_signal
 
-            raven_client = RavenClient(dsn=settings.RAVEN_CONFIG['DSN'])
+            raven_client = RavenClient(dsn=settings.RAVEN_CONFIG['dsn'])
             raven_register_logger_signal(raven_client)
             raven_register_signal(raven_client)
 

d05d710de6979cf967a94e25dc38e756bcf1a611
Jens Neuhaus <kontakt@jensneuhaus.de>
2017-03-13 13:31:48 +0100

    Automatic deploys #40

--- scripts/heroku_deploy.sh
+++ scripts/heroku_deploy.sh
@@ -1,25 +1,45 @@
 #!/bin/sh -e
 APP_NAME=$1
 
-#git remote add heroku git@heroku.com:$APP_NAME.git
-# git fetch heroku
+git remote add heroku git@heroku.com:$APP_NAME.git
+git fetch heroku
 
 MIGRATION_CHANGES=$(git diff HEAD heroku/master --name-only -- db | wc -l)
-echo "$MIGRATION_CHANGES db changes."
+echo "$MIGRATION_CHANGES db changes since last deploy."
 
 PREV_WORKERS=$(heroku ps --app $APP_NAME | grep "^worker." | wc -l | tr -d ' ')
 
 if test $MIGRATION_CHANGES -gt 0; then
+
+  DEPLOY_START=`date +%s`
+
   heroku maintenance:on --app $APP_NAME
 
   # Make sure workers are not running during a migration
-  heroku scale worker=0 --app $APP_NAME
+
+  if test $PREV_WORKERS -gt 0; then
+    heroku scale worker=0 --app $APP_NAME
+  fi
+
+  # deploy code changes (and implicitly restart the app and any running workers)
+  git push heroku $CIRCLE_SHA1:refs/heads/master
 
   heroku run python manage.py migrate --app $APP_NAME
-  heroku scale worker=$PREV_WORKERS --app $APP_NAME
+
+  if test $PREV_WORKERS -gt 0; then
+    heroku scale worker=$PREV_WORKERS --app $APP_NAME
+  fi
+
   heroku restart --app $APP_NAME
 
   heroku maintenance:off --app $APP_NAME
 
+  DEPLOY_END=`date +%s`
+  ELAPSED=$(( $DEPLOY_END - $DEPLOY_START ))
+
+  echo "Everything done. $ELAPSED seconds in maintenance mode."
+
 fi
+
+exit 0
 

0f1ab0a0618a571f312ae0993a4b056a919b0c40
Cheryl Yang <web@cheryllium.com>
2017-03-23 04:51:26 -0400

    Create PDF example at /htmltopdf/example/

--- apps/htmltopdf/models.py
+++ apps/htmltopdf/models.py
@@ -6,6 +6,7 @@ from django.contrib.postgres.fields import JSONField
 from django.template.loader import render_to_string
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils.translation import ugettext_lazy as _
 
 from weasyprint import HTML, CSS
 
@@ -16,20 +17,22 @@ class CustomPDF(models.Model):
     template = models.CharField(
         _("Name of template"),
         help_text=_("Name of template file without file extension"),
+        max_length=50, 
         null=False,
         blank=False
     )
 
     context = JSONField(
         _("Context for filling in template"),
-        help_text=_(""),
+        help_text=_("Context dictionary"),
         null=False,
         blank=False
     )
 
     output_filename = models.CharField(
         _("Name of output PDF file"),
-        help_text=_(),
+        help_text=_("Including file extension (.pdf)"),
+        max_length=50, 
         null=False,
         blank=False
     )
@@ -40,7 +43,7 @@ class CustomPDF(models.Model):
         Views calling this method must pass the request as well.
         """
 
-        rendered_html = render_to_string("{}{}.html".format(TEMPLATE_PREFIX, self.template), self.context, request)
+        rendered_html = render_to_string("{}{}.html".format(TEMPLATES_PREFIX, self.template), self.context, request)
         
         buf = BytesIO()
 
@@ -55,5 +58,5 @@ class CustomPDF(models.Model):
 
         response = HttpResponse(pdf_output, content_type='application/pdf')
         response['Content-Disposition'] = 'filename="{}"'.format(self.output_filename)
-
+        
         return response

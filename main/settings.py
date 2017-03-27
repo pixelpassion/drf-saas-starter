74eb81833d6c841921c82c96326c177451148f78
Jens Neuhaus <kontakt@jensneuhaus.de>
2017-03-27 16:09:00 +0200

    Cleaned settings

--- main/settings.py
+++ main/settings.py
@@ -2,6 +2,26 @@ import environ
 import datetime
 import sys
 
+########################################################################################################################
+#                                                            Index
+########################################################################################################################
+
+# 1     Basic
+# 2     Installed apps + middleware
+# 3     Templates + static files (CSS, JavaScript, Images)
+# 4     Deployments (Heroku)
+# 5     Authentication
+# 6     Mail handling
+# 7     Celery
+# 8     Django Rest Framework
+# 9     Context (Admin, etc.)
+# 10    Tenants
+
+
+########################################################################################################################
+#                                                      1 - Basics                                                      #
+########################################################################################################################
+
 env = environ.Env()                             # set default values and casting
 environ.Env.read_env('.env')                    # reading .env file
 
@@ -21,7 +41,6 @@ ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', [])   # Should have '*' for local, the
 SITE_ID = 1
 
 # Heroku expects to receive error messages on STDERR, the following logging takes care of this
-
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': True,
@@ -66,7 +85,26 @@ LOGGING = {
     },
 }
 
-# Application definition
+ROOT_URLCONF = 'main.urls'
+
+WSGI_APPLICATION = 'main.wsgi.application'
+
+# Internationalization
+LANGUAGE_CODE = 'de_DE'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+# Everybody needs a database, Raises ImproperlyConfigured exception if not set
+DATABASES = {
+    'default': env.db(),
+}
+
+
+########################################################################################################################
+#                                            2 - Installed apps + Middleware                                           #
+########################################################################################################################
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -115,7 +153,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-
 if DEBUG is True and STAGE == 'local':
 
         INSTALLED_APPS += [
@@ -129,7 +166,9 @@ if DEBUG is True and STAGE == 'local':
         INTERNAL_IPS = ['127.0.0.1', '10.0.2.2', ]
 
 
-ROOT_URLCONF = 'main.urls'
+########################################################################################################################
+#                                      3 - Templates + static files (CSS, JavaScript, Images)                          #
+########################################################################################################################
 
 TEMPLATES = [
     {
@@ -150,52 +189,6 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'main.wsgi.application'
-
-
-# Database
-
-DATABASES = {
-    'default': env.db(),                        # Raises ImproperlyConfigured exception if not set
-}
-
-
-# Password validation
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
-# Authentication
-
-AUTHENTICATION_BACKENDS = (
-    'django.contrib.auth.backends.ModelBackend',            # Needed to login by username in Django admin, regardless of `allauth`
-    'allauth.account.auth_backends.AuthenticationBackend',  # for specific authentication methods, such as login by e-mail
-)
-
-# Internationalization
-
-LANGUAGE_CODE = 'de_DE'
-
-TIME_ZONE = 'UTC'
-USE_I18N = True
-USE_L10N = True
-USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-
 MEDIA_ROOT = str(root.path('media'))
 MEDIA_URL = '/media/'
 STATIC_ROOT = str(root.path('staticfiles'))
@@ -214,15 +207,9 @@ if not STAGE == 'test':
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
-# Mail handling
-
-SENDGRID_API_KEY = env("SENDGRID_API_KEY")
-
-ANYMAIL = {
-    "SENDGRID_API_KEY": SENDGRID_API_KEY,
-}
-
-# Heroku
+########################################################################################################################
+#                                                4 - Deployments (Heroku)                                              #
+########################################################################################################################
 
 # heroku labs:enable runtime-dyno-metadata must have been run before + a new deploy
 ON_HEROKU = env('HEROKU_APP_ID', default=False)
@@ -259,7 +246,9 @@ if ON_HEROKU:
     }
 
 
-# Allauth
+########################################################################################################################
+#                                                5 - Authentication                                                    #
+########################################################################################################################
 
 ACCOUNT_ALLOW_REGISTRATION = env.bool('ACCOUNT_ALLOW_REGISTRATION', True)
 ACCOUNT_CONFIRM_EMAIL_ON_GET = True
@@ -273,15 +262,61 @@ ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_ADAPTER = 'apps.users.adapters.AccountAdapter'
 ACCOUNT_LOGIN_ATTEMPTS_LIMIT = 5
 ACCOUNT_LOGIN_ATTEMPTS_TIMEOUT = 300
+#ACCOUNT_DEFAULT_HTTP_PROTOCOL="https"
 
 AUTH_USER_MODEL = 'users.User'
 LOGIN_REDIRECT_URL = 'users:redirect'
 LOGIN_URL = 'account_login'
 
-#ACCOUNT_DEFAULT_HTTP_PROTOCOL="https"
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',            # Needed to login by username in Django admin, regardless of `allauth`
+    'allauth.account.auth_backends.AuthenticationBackend',  # for specific authentication methods, such as login by e-mail
+)
+
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+]
+
+# https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'OPTIONS': {
+            'max_similarity': 0.7,
+            'user_attributes': ('username', 'first_name', 'last_name', 'email')
+        }
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {
+            'min_length': 8,
+        }
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        # optional path for password black list (default path: django/contrib/auth/common-passwords.txt.gz)
+        'OPTIONS': {
+            #'password_list_path': '/path/to/passwordBlackList.txt.gz',
+        }
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    }
+]
 
 
-# Emails
+########################################################################################################################
+#                                                6 - Mail handling                                                     #
+########################################################################################################################
+
+SENDGRID_API_KEY = env("SENDGRID_API_KEY")
+
+ANYMAIL = {
+    "SENDGRID_API_KEY": SENDGRID_API_KEY,
+}
+
 EMAIL_BACKEND = env('EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
 EMAIL_PORT = env("EMAIL_PORT", default=1025)
 EMAIL_HOST = env("EMAIL_HOST", default='localhost')
@@ -292,34 +327,32 @@ DEFAULT_FROM_EMAIL="mail@example.org"
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-# Admin
 
-SENTRY_URL = env.str('SENTRY_URL', default="#")
-MAILHOG_URL = env.str('MAILHOG_URL', default="#")
-RABBITMQ_MANAGEMENT_URL = env.str('RABBITMQ_MANAGEMENT_URL', default="#")
-
-# Celery
+########################################################################################################################
+#                                                 7 - Celery                                                           #
+########################################################################################################################
 
 # In development, all tasks will be executed locally by blocking until the task returns
 
 CELERY_BROKER_URL = env.str('CLOUDAMQP_URL', default='amqp://guest:guest@127.0.0.1')
+BROKER_POOL_LIMIT = 1                   # Will decrease connection usage
+BROKER_HEARTBEAT = None                 # We're using TCP keep-alive instead
+BROKER_CONNECTION_TIMEOUT = 30          # May require a long timeout due to Linux DNS timeouts etc
+CELERY_RESULT_BACKEND = None            # AMQP is not recommended as result backend as it creates thousands of queues
+CELERY_SEND_EVENTS = False              # Will not create celeryev.* queues
+CELERY_EVENT_QUEUE_EXPIRES = 60         # Will delete all celeryev. queues without consumers after 1 minute.
 
+
+# When the setting CELERY_ALWAYS_EAGER is set to True, will all taskes be executed locally by blocking until the task
+# returns. That is, tasks will be executed locally instead of being sent to the queue. This forces all calls to
+# .delay()/.apply_async() that would normally get delegated to the worker to instead execute synchronously.
 if STAGE == 'local':
     CELERY_TASK_ALWAYS_EAGER = env.bool('CELERY_ALWAYS_EAGER', default=True)
 
-# Rest framework
 
-OAUTH2_PROVIDER = {
-    # this is the list of available scopes
-    'SCOPES': {
-        'read': 'Read scope',
-        'write': 'Write scope',
-        'groups': 'Access to your groups',
-        'special_admin': 'Access to all data ressources'
-    },
-    'ACCESS_TOKEN_EXPIRE_SECONDS': 60 * 60 * 12
-
-}
+########################################################################################################################
+#                                                 8 - Django Rest Framework                                            #
+########################################################################################################################
 
 # REST_FRAMEWORK = {
 #     'DEFAULT_AUTHENTICATION_CLASSES': (
@@ -354,7 +387,6 @@ REST_FRAMEWORK = {
     ),
 }
 
-
 JWT_SECRET = env('JWT_SECRET')       # Raises ImproperlyConfigured exception if JWT_SECRET not set
 JWT_ISSUER_NAME = env.str('JWT_ISSUER_NAME', default='einhorn-starter')
 
@@ -370,10 +402,35 @@ JWT_AUTH = {
     'JWT_ISSUER': 'einhorn-starter',
 }
 
+OAUTH2_PROVIDER = {
+    # this is the list of available scopes
+    'SCOPES': {
+        'read': 'Read scope',
+        'write': 'Write scope',
+        'groups': 'Access to your groups',
+        'special_admin': 'Access to all data ressources'
+    },
+    'ACCESS_TOKEN_EXPIRE_SECONDS': 60 * 60 * 12
+
+}
+
 REST_USE_JWT = True
+
+
+########################################################################################################################
+#                                             9 - Context (Admin, etc.)                                                #
+########################################################################################################################
 
 PROJECT_NAME = 'Project'
 
+SENTRY_URL = env.str('SENTRY_URL', default="#")
+MAILHOG_URL = env.str('MAILHOG_URL', default="#")
+RABBITMQ_MANAGEMENT_URL = env.str('RABBITMQ_MANAGEMENT_URL', default="#")
+
+
+########################################################################################################################
+#                                                 10 - Tenants                                                         #
+########################################################################################################################
 
 ALLOWED_EMAIL_DOMAINS = [
     'jensneuhaus.de',
@@ -385,41 +442,3 @@ DEFAULT_DOMAINS = env.list('DEFAULT_DOMAINS',default=[
     'www.localhost:8000',
     '127.0.0.1:8000',
 ])
-
-PASSWORD_HASHERS = [
-    'django.contrib.auth.hashers.Argon2PasswordHasher',
-    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
-    'django.contrib.auth.hashers.BCryptPasswordHasher',
-]
-
-# Password validation
-# https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
-
-AUTH_USER_MODEL = 'users.User'
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-        'OPTIONS': {
-            'max_similarity': 0.7,
-            'user_attributes': ('username', 'first_name', 'last_name', 'email')
-        }
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-        'OPTIONS': {
-            'min_length': 8,
-        }
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-        # optional path for password black list (default path: django/contrib/auth/common-passwords.txt.gz)
-        'OPTIONS': {
-            #'password_list_path': '/path/to/passwordBlackList.txt.gz',
-        }
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    }
-]
-

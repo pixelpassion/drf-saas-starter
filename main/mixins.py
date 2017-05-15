873873cecccd42b8c79e922e9cdd1b1da9500f03
Jens Neuhaus <kontakt@jensneuhaus.de>
2017-05-15 15:07:23 +0200

    Fixing Mixin docstrings

--- main/mixins.py
+++ main/mixins.py
@@ -14,38 +14,34 @@ class CreateMixin(models.Model):
     created = models.DateTimeField(_('Created'), auto_now_add=True)
 
     class Meta:
-        """ """
         abstract = True
         get_latest_by = 'created'
         ordering = ('created',)
 
 
 class CreateUpdateMixin(CreateMixin):
-    """Stores timestamps for creation AND last modification."""
+    """Stores timestamps for the last modification."""
 
     updated = models.DateTimeField(_('Modified'), auto_now=True)
 
     class Meta:
-        """ """
         abstract = True
 
 
 class ValidFromUntilMixin(models.Model):
-    """Stores timestamps for creation AND last modification."""
+    """Valid from / until date fields """
 
     valid_from_inclusive = models.DateField(_('valid_from'), auto_now_add=True, null=False, blank=False)
     valid_until_exclusive = models.DateField(_('valid until'), null=True, blank=True)
 
     class Meta:
-        """ """
         abstract = True
 
 
 class UUIDMixin(models.Model):
-    """Stores timestamps for creation AND last modification."""
+    """ Uses an UUID Field as an primary ID """
 
     id = models.UUIDField(_('ID'), primary_key=True, unique=True, default=uuid.uuid4, editable=False)
 
     class Meta:
-        """ """
         abstract = True
